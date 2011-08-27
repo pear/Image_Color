ae130396859c7cbb3e15d02bc1f2f561db9f67e8
Daniel O'Connor <clockwerx@php.net>
2011-08-27 01:43:18 +0000

    Upgrade to phpunit 3.5 compatibility
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_Color/trunk@315603 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,7 +3,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'AllTests::main');
 }
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'PHPUnit/Autoload.php';
 
 require_once 'ColorTest.php';
 
